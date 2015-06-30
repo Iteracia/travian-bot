d77cb857340e1ac199d1fe99a00afa046075bcab
it3r <1it3era4c1ia@gmail.com>
2015-06-30 14:38:56 +0300

    settings

---
+++ src/com/company/Settings.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.io.*;
+
+public class Settings {
+    public static String login;
+    public static String password;
+    public static final boolean printAllBuildings = true;
+
+    static {
+        File info = new File( "./settings.txt" );
+        try ( BufferedReader br = new BufferedReader(new FileReader(info)) ) {
+            login = br.readLine();
+            password = br.readLine();
+        }catch ( Exception e ){
+            System.out.println("failed to load login and password");
+            login = "";
+            password = "";
+        }
+    }
+}
